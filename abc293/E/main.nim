c5b5e4e56c9c0480d3c1f644f9e94062ab683418
Toshihiro Shimizu <toshihiroshimizu@ToshihironoMac-mini.local>
2023-03-17 01:31:24 +0900

    hoge

--- abc293/E/main.nim
+++ abc293/E/main.nim
@@ -6,12 +6,23 @@ const
   USE_DEFAULT_TABLE = true
 
 include lib/header/chaemon_header
+import atcoder/modint
+import lib/math/matrix
+type mint = modint
 
 solveProc solve(A:int, X:int, M:int):
+  mint.setMod(M)
+  type MT = StaticMatrixType(mint)
+  var
+    A = MT.init([[A, 0], [1, 1]])
+    b = MT.initVector([1, 0])
+  b = A^X * b
+  echo b[1]
   discard
 
 when not defined(DO_TEST):
-  
+  let A, X, M = nextInt()
+  solve(A, X, M)
 else:
   discard
 
--- abc293/F/main.nim
+++ abc293/F/main.nim
@@ -6,13 +6,55 @@ const
   USE_DEFAULT_TABLE = true
 
 include lib/header/chaemon_header
+import lib/other/bitutils
+import lib/other/binary_search
+
+const B = 5
 
 # Failed to predict input format
-solveProc solve():
+solveProc solve(N:int):
+  # B桁より大きいものを数える
+  ans := 0
+  block:
+    b := 2
+    while true:
+      var
+        N = N
+        ok = true
+        ct = 0
+      while N > 0:
+        let d = N mod b
+        if d >= 2: ok = false
+        ct.inc
+        N = N div b
+      if ct <= B: break
+      if ok:
+        ans.inc
+      b.inc
+  # 1 ..< 2^Bについて2分探索
+  # ２進数表記でtになる
+  for t in 2^B:
+    proc base(b:int):int =
+      p := 1
+      s := 0
+      for i in B:
+        if t[i] == 1:
+          s = s +! p
+        p = p *! b
+      return s
+    proc f(b:int):bool =
+      return N <= base(b)
+    let b = f.minLeft(2 .. N + 1)
+    if base(b) == N:
+      ans.inc
+  echo ans
   discard
 
 when not defined(DO_TEST):
-  solve()
+  let T = nextInt()
+  for _ in T:
+    let N = nextInt()
+    solve(N)
 else:
   discard
 
